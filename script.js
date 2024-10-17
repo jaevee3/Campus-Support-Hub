bf97dee05a605898dc103a00c3d9158c98c2ee36
jaevee3 <jvillarreal680@student.alamo.edu>
2024-10-16 21:04:46 -0500

    Update script.js

--- script.js
+++ script.js
@@ -21,24 +21,3 @@ function openTab(evt, tabName) {
     // Add "active" class to the clicked link
     evt.currentTarget.classList.add("active");
 }
-
-// Smooth scroll for navigation links
-document.querySelectorAll('nav a').forEach(anchor => {
-    anchor.addEventListener('click', function(e) {
-        e.preventDefault();
-        const targetId = this.getAttribute('href').replace('#', '');
-        const targetElement = document.getElementById(targetId);
-        if (targetElement) {
-            targetElement.scrollIntoView({ behavior: 'smooth' });
-        }
-    });
-});
-
-// Event listener to handle tab display when navigation links are clicked
-document.querySelectorAll('nav ul li a').forEach(link => {
-    link.addEventListener('click', function(event) {
-        const tabName = this.getAttribute('href').replace('#', '');
-        openTab(event, tabName);
-    });
-});
-
